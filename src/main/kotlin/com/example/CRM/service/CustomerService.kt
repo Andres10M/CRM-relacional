390f3784ee47a9545c4354b7fe2f321a655fcd62
Andres10M <pasari@sudamericano.edu.ec>
2024-11-23 17:41:23 -0500

    primer commit

--- src/main/kotlin/com/example/CRM/service/CustomerService.kt
+++ src/main/kotlin/com/example/CRM/service/CustomerService.kt
@@ -1,36 +1,26 @@
 package com.example.CRM.service
 
+
 import com.example.CRM.dto.CustomerDTO
-import com.example.CRM.model.Customer
+import com.example.CRM.mapper.CustomerMapper
 import com.example.CRM.repository.CustomerRepository
 import org.springframework.stereotype.Service
 
 @Service
 class CustomerService(private val customerRepository: CustomerRepository) {
 
-    fun getAllCustomers(): List<CustomerDTO> =
-        customerRepository.findAll().map { customer ->
-            CustomerDTO(customer.id, customer.name)
-        }
-
-    fun getCustomerById(id: Long): CustomerDTO? =
-        customerRepository.findById(id).map { customer ->
-            CustomerDTO(customer.id, customer.name)
-        }.orElse(null)
-
-    fun createCustomer(customerDTO: CustomerDTO): CustomerDTO {
-        val customer = Customer(name = customerDTO.name)
-        val savedCustomer = customerRepository.save(customer)
-        return CustomerDTO(savedCustomer.id, savedCustomer.name)
+    fun getAllCustomers(): List<CustomerDTO> {
+        return customerRepository.findAll().map { CustomerMapper.toDto(it) }
     }
 
-    fun updateCustomer(id: Long, customerDTO: CustomerDTO): CustomerDTO? {
-        return if (customerRepository.existsById(id)) {
-            val customer = Customer(id = id, name = customerDTO.name)
-            val updatedCustomer = customerRepository.save(customer)
-            CustomerDTO(updatedCustomer.id, updatedCustomer.name)
-        } else {
-            null
-        }
+    fun createCustomer(customerDto: CustomerDTO): CustomerDTO {
+        val customer = CustomerMapper.toEntity(customerDto)
+        val savedCustomer = customerRepository.save(customer)
+        return CustomerMapper.toDto(savedCustomer)
+    }
+
+    fun getCustomerById(id: Long): CustomerDTO{
+        val customer = customerRepository.findById(id).orElseThrow { Exception("Customer not found") }
+        return CustomerMapper.toDto(customer)
     }
 }
